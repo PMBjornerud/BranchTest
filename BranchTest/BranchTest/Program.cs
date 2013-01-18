055d982900923e6e5f36946ca6fc36890da89a13
PMBjornerud <PublicBjornerud@gmail.com>
2013-01-18 13:36:13 -0800

    Initial commit

---
+++ BranchTest/BranchTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BranchTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial commit");
+            Console.ReadKey();
+        }
+    }
+}
